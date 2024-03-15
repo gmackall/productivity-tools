8437986733f0292101e6ea8438888c599fc64403
Gray Mackall <mackall@google.com>
2024-03-14 23:55:05 -0700

    intermediate before deleting stuff

--- issue_grapher/python/graph_results.py
+++ issue_grapher/python/graph_results.py
@@ -20,4 +20,16 @@ df['created_at']=pandas.to_datetime(df['created_at'])
 #print(df2)
 df3 = df.groupby([pandas.Grouper(key='created_at', freq='W')]).count()
 
-df3['id'].plot(kind='bar').figure.savefig(f'./outputs/{args.issue}.png')
+# Show date for at most 10 bars on x axis
+tick_step_size = round(len(df3['id']) / 10)
+
+# plot
+fig, ax = plt.subplots()
+df3['id'].plot(kind="bar", stacked=True, ax=ax)
+xLabels = ['']*len(df3['id'])
+xLabels[::tick_step_size] = [x.strftime("%Y-%m-%d") for x in df3['id'].index[::tick_step_size]]
+#ax.set_xticklabels([x.strftime("%Y-%m-%d") for x in df3['id'].index], rotation=45)
+ax.set_xticklabels(xLabels, rotation=45)
+plt.subplots_adjust(bottom=0.2)
+plt.savefig(f'./outputs/{args.issue}.png')
+print(len(df3['id']))
