1b67c0f20c9e2087d999640bbf31b0e3828e27d5
Gray Mackall <mackall@google.com>
2024-03-14 23:58:10 -0700

    improve formatting for dates

--- issue_grapher/python/graph_results.py
+++ issue_grapher/python/graph_results.py
@@ -13,23 +13,26 @@ parser.add_argument('--issue')
 
 args = parser.parse_args()
 
+# Load the json containing the github issue data.
 dictionary = json.load(open(f'./logs/{args.issue}.json', 'r'))
 df = pandas.DataFrame(data=dictionary)
+
+# Cast created_at from string to pandas date and then group by week.
+# TODO: this might be offset by a week in the future.
 df['created_at']=pandas.to_datetime(df['created_at'])
-#df2 = df.groupby(by=df['created_at'].dt.date).count()
-#print(df2)
 df3 = df.groupby([pandas.Grouper(key='created_at', freq='W')]).count()
+
+fig, ax = plt.subplots()
 
 # Show date for at most 10 bars on x axis
 tick_step_size = round(len(df3['id']) / 10)
-
-# plot
-fig, ax = plt.subplots()
 df3['id'].plot(kind="bar", stacked=True, ax=ax)
 xLabels = ['']*len(df3['id'])
 xLabels[::tick_step_size] = [x.strftime("%Y-%m-%d") for x in df3['id'].index[::tick_step_size]]
-#ax.set_xticklabels([x.strftime("%Y-%m-%d") for x in df3['id'].index], rotation=45)
 ax.set_xticklabels(xLabels, rotation=45)
+
+# Add padding so the dates don't get cut off.
 plt.subplots_adjust(bottom=0.2)
+
+# Save to outputs folder.
 plt.savefig(f'./outputs/{args.issue}.png')
-print(len(df3['id']))
