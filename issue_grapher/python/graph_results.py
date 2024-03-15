e85599091e66ae3e5ee70aec668a14b09a29b601
Gray Mackall <mackall@google.com>
2024-03-14 18:35:32 -0700

    remove print in python

--- issue_grapher/python/graph_results.py
+++ issue_grapher/python/graph_results.py
@@ -19,6 +19,5 @@ df['created_at']=pandas.to_datetime(df['created_at'])
 #df2 = df.groupby(by=df['created_at'].dt.date).count()
 #print(df2)
 df3 = df.groupby([pandas.Grouper(key='created_at', freq='W')]).count()
-print(df3['id'])
 
 df3['id'].plot(kind='bar').figure.savefig(f'./outputs/{args.issue}.png')
